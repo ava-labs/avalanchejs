64d0f1c8a909e36ca1f2ca813bf6f0e2884a4416
Gabriel Cardona <gabriel.cardona@avalabs.org>
2020-06-27 12:24:54 -0700

    More Payload tests.

--- tests/utils/payload.test.ts
+++ tests/utils/payload.test.ts
@@ -1,5 +1,5 @@
 import { Buffer } from "buffer/";
-import { UTF8Payload, PayloadTypes, BINPayload, HEXSTRPayload, B58STRPayload, B64STRPayload, BIGNUMPayload, XCHAINPayload, PCHAINPayload, CCHAINPayload, TXIDPayload, ASSETIDPayload, UTXOIDPayload, NFTIDPayload, SUBNETIDPayload, CHAINIDPayload } from "src";
+import { UTF8Payload, PayloadTypes, BINPayload, HEXSTRPayload, B58STRPayload, B64STRPayload, BIGNUMPayload, XCHAINPayload, PCHAINPayload, CCHAINPayload, TXIDPayload, ASSETIDPayload, UTXOIDPayload, NFTIDPayload, SUBNETIDPayload, CHAINIDPayload, NODEIDPayload, SECPSIGPayload, SECPENCPayload, JPEGPayload, PNGPayload, BMPPayload, ICOPayload, SVGPayload, CSVPayload, JSONPayload, PROTOBUFPayload, YAMLPayload } from "src";
 
 describe("Payload", () => {
     // beforeAll(() => {
@@ -163,5 +163,113 @@ describe("Payload", () => {
 
         expect(chainid.typeID()).toBe(14);
         expect(chainid.typeName()).toBe("CHAINID");
+    });
+
+    test("NODEIDPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let nodeid:NODEIDPayload = new NODEIDPayload(serialized);
+
+        expect(nodeid.typeID()).toBe(15);
+        expect(nodeid.typeName()).toBe("NODEID");
+    });
+
+    test("SECPSIGPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let secpsig:SECPSIGPayload = new SECPSIGPayload(serialized);
+
+        expect(secpsig.typeID()).toBe(16);
+        expect(secpsig.typeName()).toBe("SECPSIG");
+    });
+
+    test("SECPENCPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let secpenc:SECPENCPayload = new SECPENCPayload(serialized);
+
+        expect(secpenc.typeID()).toBe(17);
+        expect(secpenc.typeName()).toBe("SECPENC");
+    });
+
+    test("JPEGPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let jpeg:JPEGPayload = new JPEGPayload(serialized);
+
+        expect(jpeg.typeID()).toBe(18);
+        expect(jpeg.typeName()).toBe("JPEG");
+    });
+
+    test("PNGPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let png:PNGPayload = new PNGPayload(serialized);
+
+        expect(png.typeID()).toBe(19);
+        expect(png.typeName()).toBe("PNG");
+    });
+
+    test("BMPPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let bmp:BMPPayload = new BMPPayload(serialized);
+
+        expect(bmp.typeID()).toBe(20);
+        expect(bmp.typeName()).toBe("BMP");
+    });
+
+    test("ICOPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let ico:ICOPayload = new ICOPayload(serialized);
+
+        expect(ico.typeID()).toBe(21);
+        expect(ico.typeName()).toBe("ICO");
+    });
+
+    test("SVGPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let svg:SVGPayload = new SVGPayload(serialized);
+
+        expect(svg.typeID()).toBe(22);
+        expect(svg.typeName()).toBe("SVG");
+    });
+
+    test("CSVPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let csv:CSVPayload = new CSVPayload(serialized);
+
+        expect(csv.typeID()).toBe(23);
+        expect(csv.typeName()).toBe("CSV");
+    });
+
+    test("JSONPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let json:JSONPayload = new JSONPayload(serialized);
+
+        expect(json.typeID()).toBe(24);
+        expect(json.typeName()).toBe("JSON");
+    });
+
+    test("PROTOBUFPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let protobuf:PROTOBUFPayload = new PROTOBUFPayload(serialized);
+
+        expect(protobuf.typeID()).toBe(25);
+        expect(protobuf.typeName()).toBe("PROTOBUF");
+    });
+
+    test("YAMLPayload", () => {
+        let str:string = "Avalanche.js";
+        let serialized:Buffer = Buffer.from(str)
+        let yaml:YAMLPayload = new YAMLPayload(serialized);
+
+        expect(yaml.typeID()).toBe(26);
+        expect(yaml.typeName()).toBe("YAML");
     });
 });
