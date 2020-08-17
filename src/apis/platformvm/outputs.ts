be949992651302064d507a43be3be441f0a76a5b
Collin K. Cusce <collin@avalabs.org>
2020-08-17 13:39:47 -0400

    added lots of platform tests, all pass

--- src/apis/platformvm/outputs.ts
+++ src/apis/platformvm/outputs.ts
@@ -3,10 +3,9 @@
  * @module API-PlatformVM-Outputs
  */
 import { Buffer } from 'buffer/';
-import BN from 'bn.js';
 import BinTools from '../../utils/bintools';
 import { PlatformVMConstants } from './constants';
-import { Output, StandardAmountOutput, StandardTransferableOutput, BaseNFTOutput } from '../../common/output';
+import { Output, StandardAmountOutput, StandardTransferableOutput } from '../../common/output';
 
 const bintools = BinTools.getInstance();
 
@@ -21,13 +20,7 @@ export const SelectOutputClass = (outputid:number, ...args:Array<any>):Output =>
     if(outputid == PlatformVMConstants.SECPOUTPUTID){
         let secpout:SecpOutput = new SecpOutput( ...args);
         return secpout;
-    } else if(outputid == PlatformVMConstants.NFTMINTOUTPUTID){
-        let nftout:NFTMintOutput = new NFTMintOutput(...args);
-        return nftout;
-    } else if(outputid == PlatformVMConstants.NFTXFEROUTPUTID){
-        let nftout:NFTTransferOutput = new NFTTransferOutput(...args);
-        return nftout;
-    }
+    } 
     throw new Error("Error - SelectOutputClass: unknown outputid " + outputid);
 }
 
@@ -52,16 +45,6 @@ export abstract class AmountOutput extends StandardAmountOutput {
     }
 }
 
-export abstract class NFTOutput extends BaseNFTOutput {
-    /**
-     * 
-     * @param assetID An assetID which is wrapped around the Buffer of the Output
-     */
-    makeTransferable(assetID:Buffer):TransferableOutput {
-        return new TransferableOutput(assetID, this);
-    }
-}
-
 /**
  * An [[Output]] class which specifies an Output that carries an ammount for an assetID and uses secp256k1 signature scheme.
  */
@@ -74,112 +57,3 @@ export class SecpOutput extends AmountOutput {
   }
 }
 
-/**
- * An [[Output]] class which specifies an Output that carries an NFT Mint and uses secp256k1 signature scheme.
- */
-export class NFTMintOutput extends NFTOutput {
-    /**
-     * Returns the outputID for this output
-     */
-    getOutputID():number {
-        return PlatformVMConstants.NFTMINTOUTPUTID;
-    }
-
-    /**
-     * Popuates the instance from a {@link https://github.com/feross/buffer|Buffer} representing the [[NFTMintOutput]] and returns the size of the output.
-     */
-    fromBuffer(utxobuff:Buffer, offset:number = 0):number {
-        this.groupID = bintools.copyFrom(utxobuff, offset, offset + 4);
-        offset += 4;
-        return super.fromBuffer(utxobuff, offset);
-    }
-
-    /**
-     * Returns the buffer representing the [[NFTMintOutput]] instance.
-     */
-    toBuffer():Buffer {
-        let superbuff:Buffer = super.toBuffer();
-        let bsize:number = this.groupID.length + superbuff.length;
-        let barr:Array<Buffer> = [this.groupID, superbuff];
-        return Buffer.concat(barr,bsize);
-    }
-
-    /**
-     * An [[Output]] class which contains an NFT mint for an assetID.
-     * 
-     * @param groupID A number specifies the group this NFT is issued to
-     * @param locktime A {@link https://github.com/indutny/bn.js/|BN} representing the locktime
-     * @param threshold A number representing the the threshold number of signers required to sign the transaction
-     * @param addresses An array of {@link https://github.com/feross/buffer|Buffer}s representing addresses
-     */
-    constructor(groupID:number = undefined, addresses:Array<Buffer> = undefined, locktime:BN = undefined, threshold:number = undefined){
-        super(addresses, locktime, threshold);
-        if(typeof groupID !== 'undefined') {
-            this.groupID.writeUInt32BE(groupID, 0);
-        }
-    }
-}
-
-/**
- * An [[Output]] class which specifies an Output that carries an NFT and uses secp256k1 signature scheme.
- */
-export class NFTTransferOutput extends NFTOutput {
-    protected sizePayload:Buffer = Buffer.alloc(4);
-    protected payload:Buffer;
-
-    /**
-     * Returns the outputID for this output
-     */
-    getOutputID():number {
-        return PlatformVMConstants.NFTXFEROUTPUTID;
-    }
-
-  /**
-     * Returns the payload as a {@link https://github.com/feross/buffer|Buffer}
-     */
-  getPayload = ():Buffer => bintools.copyFrom(this.payload);
-
-    /**
-     * Popuates the instance from a {@link https://github.com/feross/buffer|Buffer} representing the [[NFTTransferOutput]] and returns the size of the output.
-     */
-    fromBuffer(utxobuff:Buffer, offset:number = 0):number {
-        this.groupID = bintools.copyFrom(utxobuff, offset, offset + 4);
-        offset += 4;
-        this.sizePayload = bintools.copyFrom(utxobuff, offset, offset + 4);
-        let psize:number = this.sizePayload.readUInt32BE(0);
-        offset += 4;
-        this.payload = bintools.copyFrom(utxobuff, offset, offset + psize);
-        offset = offset + psize;
-        return super.fromBuffer(utxobuff, offset);
-    }
-
-    /**
-     * Returns the buffer representing the [[NFTTransferOutput]] instance.
-     */
-  toBuffer():Buffer {
-    const superbuff:Buffer = super.toBuffer();
-    const bsize:number = this.groupID.length + this.sizePayload.length + this.payload.length + superbuff.length;
-    this.sizePayload.writeUInt32BE(this.payload.length, 0);
-    const barr:Array<Buffer> = [this.groupID, this.sizePayload, this.payload, superbuff];
-    return Buffer.concat(barr, bsize);
-  }
-
-  /**
-     * An [[Output]] class which contains an NFT on an assetID.
-     *
-     * @param groupID A number representing the amount in the output
-     * @param payload A {@link https://github.com/feross/buffer|Buffer} of max length 1024 
-     * @param addresses An array of {@link https://github.com/feross/buffer|Buffer}s representing addresses
-     * @param locktime A {@link https://github.com/indutny/bn.js/|BN} representing the locktime
-     * @param threshold A number representing the the threshold number of signers required to sign the transaction
-
-     */
-  constructor(groupID:number = undefined, payload:Buffer = undefined, addresses:Array<Buffer> = undefined, locktime:BN = undefined, threshold:number = undefined, ) {
-    super(addresses, locktime, threshold);
-    if (typeof groupID !== 'undefined' && typeof payload !== 'undefined') {
-      this.groupID.writeUInt32BE(groupID, 0);
-      this.sizePayload.writeUInt32BE(payload.length, 0);
-      this.payload = bintools.copyFrom(payload, 0, payload.length);
-    }
-  }
-}
---
+++ tests/apis/platformvm/outputs.test.ts
@@ -0,0 +1,75 @@
+import BN from 'bn.js';
+import { Buffer } from 'buffer/';
+import BinTools from 'src/utils/bintools';
+import { SecpOutput, SelectOutputClass } from 'src/apis/platformvm/outputs';
+import { Output } from 'src/common/output';
+
+const bintools = BinTools.getInstance();
+
+describe('Outputs', () => {
+
+    describe('SecpOutput', () => {
+      let addrs:Array<Buffer> = [
+          bintools.cb58Decode("B6D4v1VtPYLbiUvYXtW4Px8oE9imC2vGW"),
+          bintools.cb58Decode("P5wdRuZeaDt28eHMP5S3w9ZdoBfo7wuzF"),
+          bintools.cb58Decode("6Y3kysjF9jnHnYkdS9yGAuoHyae2eNmeV")
+      ].sort();
+
+      let locktime:BN = new BN(54321);
+      let addrpay = [addrs[0], addrs[1]];
+      let fallLocktime:BN = locktime.add(new BN(50));
+
+      test('SelectOutputClass', () => {
+          let goodout:SecpOutput = new SecpOutput(new BN(2600), addrpay, fallLocktime, 1);
+          let outpayment:Output = SelectOutputClass(goodout.getOutputID());
+          expect(outpayment).toBeInstanceOf(SecpOutput);
+          expect(() => {
+              SelectOutputClass(99);
+          }).toThrow("Error - SelectOutputClass: unknown outputid");
+      });
+
+      test('comparator', () => {
+          let outpayment1:Output = new SecpOutput(new BN(10000), addrs, locktime, 3);
+          let outpayment2:Output = new SecpOutput(new BN(10001), addrs, locktime, 3);
+          let outpayment3:Output = new SecpOutput(new BN(9999), addrs, locktime, 3);
+          let cmp = Output.comparator();
+          expect(cmp(outpayment1, outpayment1)).toBe(0);
+          expect(cmp(outpayment2, outpayment2)).toBe(0);
+          expect(cmp(outpayment3, outpayment3)).toBe(0);
+          expect(cmp(outpayment1, outpayment2)).toBe(-1);
+          expect(cmp(outpayment1, outpayment3)).toBe(1);
+      });
+
+      test('SecpOutput', () => {
+          let out:SecpOutput = new SecpOutput(new BN(10000), addrs, locktime, 3);
+          expect(out.getOutputID()).toBe(7);
+          expect(JSON.stringify(out.getAddresses().sort())).toStrictEqual(JSON.stringify(addrs.sort()));
+
+          expect(out.getThreshold()).toBe(3);
+          expect(out.getLocktime().toNumber()).toBe(locktime.toNumber());
+
+          let r = out.getAddressIdx(addrs[2]);
+          expect(out.getAddress(r)).toStrictEqual(addrs[2]);
+          expect(() => {
+              out.getAddress(400)
+          }).toThrow();
+
+          expect(out.getAmount().toNumber()).toBe(10000);
+
+          let b:Buffer = out.toBuffer();
+          expect(out.toString()).toBe(bintools.bufferToB58(b));
+
+          let s:Array<Buffer> = out.getSpenders(addrs);
+          expect(JSON.stringify(s.sort())).toBe(JSON.stringify(addrs.sort()));
+
+          let m1:boolean = out.meetsThreshold([addrs[0]]);
+          expect(m1).toBe(false);
+          let m2:boolean = out.meetsThreshold(addrs, new BN(100));
+          expect(m2).toBe(false);
+          let m3:boolean = out.meetsThreshold(addrs);
+          expect(m3).toBe(true);
+          let m4:boolean = out.meetsThreshold(addrs, locktime.add(new BN(100)));
+          expect(m4).toBe(true);
+      });
+    });
+});
