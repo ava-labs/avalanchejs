5b3fd7e0c6d990fe4bf5f736d37e4aa4715cc5f9
Collin K. Cusce <collin@avalabs.org>
2020-09-17 21:32:04 -0400

    testy testy

--- src/apis/platformvm/outputs.ts
+++ src/apis/platformvm/outputs.ts
@@ -7,6 +7,7 @@ import BinTools from '../../utils/bintools';
 import { PlatformVMConstants } from './constants';
 import { Output, StandardAmountOutput, StandardTransferableOutput, StandardParseableOutput } from '../../common/output';
 import { Serialization, SerializedEncoding } from '../../utils/serialization';
+import BN from 'bn.js';
 
 const bintools = BinTools.getInstance();
 const serializer = Serialization.getInstance();
@@ -114,6 +115,82 @@ export class SECPTransferOutput extends AmountOutput {
     return newout as this;
   }
 }
+
+/**
+ * An [[Output]] class which specifies an input that has a locktime which can also enable staking of the value held, preventing transfers but not validation.
+ */
+export class StakeableLockOut extends SECPTransferOutput {
+  protected _typeName = "StakeableLockOut";
+  protected _typeID = PlatformVMConstants.STAKEABLELOCKOUTID;
+
+  //serialize and deserialize both are inherited
+
+  serialize(encoding:SerializedEncoding = "hex"):object {
+    let fields:object = super.serialize(encoding);
+    return {
+      ...fields,
+      "stakeableLocktime": serializer.encoder(this.stakeableLocktime, encoding, "Buffer", "decimalString", 8)
+    }
+  };
+  deserialize(fields:object, encoding:SerializedEncoding = "hex") {
+    super.deserialize(fields, encoding);
+    this.stakeableLocktime = serializer.decoder(fields["stakeableLocktime"], encoding, "decimalString", "Buffer", 8);
+  }
+
+  protected stakeableLocktime:Buffer;
+  /**
+   * Popuates the instance from a {@link https://github.com/feross/buffer|Buffer} representing the [[StakeableLockOut]] and returns the size of the output.
+   */
+  fromBuffer(outbuff:Buffer, offset:number = 0):number {
+    this.stakeableLocktime = bintools.copyFrom(outbuff, offset, offset + 8);
+    offset += 8;
+    return super.fromBuffer(outbuff, offset);
+  }
+
+  /**
+   * Returns the buffer representing the [[StakeableLockOut]] instance.
+   */
+  toBuffer():Buffer {
+    const superbuff:Buffer = super.toBuffer();
+    const bsize:number = this.stakeableLocktime.length + superbuff.length;
+    const barr:Array<Buffer> = [this.stakeableLocktime, superbuff];
+    return Buffer.concat(barr, bsize);
+  }
+
+  /**
+   * Returns the outputID for this output
+   */
+  getOutputID():number {
+    return this._typeID;
+  }
+
+  create(...args:any[]):this{
+    return new SECPTransferOutput(...args) as this;
+  }
+
+  clone():this {
+    const newout:SECPTransferOutput = this.create()
+    newout.fromBuffer(this.toBuffer());
+    return newout as this;
+  }
+
+  /**
+   * A [[StandardAmountOutput]] class which specifies an [[Output]] that has a locktime which can also enable staking of the value held, preventing transfers but not validation.
+   *
+   * @param stakeableLocktime A {@link https://github.com/indutny/bn.js/|BN} representing the stakeable locktime
+   * @param amount A {@link https://github.com/indutny/bn.js/|BN} representing the amount in the output
+   * @param addresses An array of {@link https://github.com/feross/buffer|Buffer}s representing addresses
+   * @param locktime A {@link https://github.com/indutny/bn.js/|BN} representing the locktime
+   * @param threshold A number representing the the threshold number of signers required to sign the transaction
+   */
+  constructor(stakeableLocktime:BN = undefined, amount:BN = undefined, addresses:Array<Buffer> = undefined, locktime:BN = undefined, threshold:number = undefined) {
+    super(amount, addresses, locktime, threshold);
+    if (typeof stakeableLocktime !== undefined) {
+      this.stakeableLocktime = bintools.fromBNToBuffer(stakeableLocktime, 8);
+    }
+  }
+}
+
 
 /**
  * An [[Output]] class which only specifies an Output ownership and uses secp256k1 signature scheme.
