a8db258344bcacbe8e29fae045b072e3f9239e48
Gabriel Cardona <gabriel@avalabs.org>
2020-10-28 10:41:01 -0700

    Add support for the new includeReason property of platformvm.getTxStatus.

--- tests/apis/platformvm/api.test.ts
+++ tests/apis/platformvm/api.test.ts
@@ -714,7 +714,7 @@ describe('PlatformVMAPI', () => {
   test('getTxStatus', async () => {
     const txid:string = 'f966750f438867c3c9828ddcdbe660e21ccdbb36a9276958f011ba472f75d4e7';
 
-    const result:Promise<string> = api.getTxStatus(txid);
+    const result:Promise<object> = api.getTxStatus(txid);
     const payload:object = {
       result:  'accepted'
     };
@@ -723,7 +723,7 @@ describe('PlatformVMAPI', () => {
     };
 
     mockAxios.mockResponse(responseObj);
-    const response:string = await result;
+    const response:object = await result;
 
     expect(mockAxios.request).toHaveBeenCalledTimes(1);
     expect(response).toBe('accepted');
