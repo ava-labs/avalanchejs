d062937e00723c63b64e94c3a1b7a2f768922028
StephenButtolph <stephen@avalabs.org>
2020-11-05 17:53:53 -0500

    added comments to AssetAmount

--- src/common/assetamount.ts
+++ src/common/assetamount.ts
@@ -12,13 +12,27 @@ import { StandardTransferableInput } from './input';
  * Class for managing asset amounts in the UTXOSet fee calcuation
  */
 export class AssetAmount {
+  // assetID that is amount is managing.
   protected assetID: Buffer = Buffer.alloc(32);
+  // amount of this asset that should be sent.
   protected amount: BN = new BN(0);
+  // burn is the amount of this asset that should be burned.
   protected burn: BN = new BN(0);
+
+  // spent is the total amount of this asset that has been consumed.
   protected spent: BN = new BN(0);
-  protected change: BN = new BN(0);
+  // stakeableLockSpent is the amount of this asset that has been consumed that
+  // was locked.
   protected stakeableLockSpent: BN = new BN(0);
+
+  // change is the excess amount of this asset that was consumed over the amount
+  // requested to be consumed(amount + burn).
+  protected change: BN = new BN(0);
+  // stakeableLockChange is a flag to mark if the input that generated the
+  // change was locked.
   protected stakeableLockChange: boolean = false;
+
+  // finished is a convenience flag to track "spent >= amount + burn"
   protected finished: boolean = false;
 
   getAssetID = (): Buffer => {
@@ -57,20 +71,26 @@ export class AssetAmount {
     return this.finished;
   }
 
+  // spendAmount should only be called if this asset is still awaiting more
+  // funds to consume.
   spendAmount = (amt: BN, stakeableLocked: boolean = false): boolean => {
-    if (!this.finished) {
-      let total: BN = this.amount.add(this.burn);
-      this.spent = this.spent.add(amt);
+    if (this.finished) {
+      /* istanbul ignore next */
+      throw new Error('Error - AssetAmount.spendAmount: attempted to spend '
+        + 'excess funds');
+    }
+    this.spent = this.spent.add(amt);
+    if (stakeableLocked) {
+      this.stakeableLockSpent = this.stakeableLockSpent.add(amt);
+    }
+
+    const total: BN = this.amount.add(this.burn);
+    if (this.spent.gte(total)) {
+      this.change = this.spent.sub(total);
       if (stakeableLocked) {
-        this.stakeableLockSpent = this.stakeableLockSpent.add(amt);
+        this.stakeableLockChange = true;
       }
-      if (this.spent.gte(total)) {
-        this.change = this.spent.sub(total);
-        if (stakeableLocked) {
-          this.stakeableLockChange = true;
-        }
-        this.finished = true;
-      }
+      this.finished = true;
     }
     return this.finished;
   }
