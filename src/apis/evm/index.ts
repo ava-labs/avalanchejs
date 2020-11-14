c473aa39fbd6132bbb2492f9c0eebe46c8f5c43a
Gabriel Cardona <gabriel@avalabs.org>
2020-11-14 07:38:58 -0800

    EVMInput class for C-Chain TX Serialization.

--- src/apis/evm/index.ts
+++ src/apis/evm/index.ts
@@ -1,2 +1,3 @@
 export * from './api';
+export * from './inputs';
 export * from './outputs';
--- src/apis/evm/inputs.ts
+++ src/apis/evm/inputs.ts
@@ -7,11 +7,13 @@ import BinTools from '../../utils/bintools';
  */
 const bintools = BinTools.getInstance();
 
-export class EVMOutput {
+export class EVMInput {
   protected address: Buffer = Buffer.alloc(20); 
   protected amount: Buffer = Buffer.alloc(8);
   protected amountValue: BN = new BN(0);
   protected assetid: Buffer = Buffer.alloc(32);
+  protected nonce: Buffer = Buffer.alloc(8);
+  protected nonceValue: BN = new BN(0);
 
   /**
    * Returns the assetID of the input as {@link https://github.com/feross/buffer|Buffer}
@@ -27,19 +29,24 @@ export class EVMOutput {
    * Returns the assetID of the input as {@link https://github.com/feross/buffer|Buffer}
    */ 
   getAssetID = (): Buffer => this.assetid;
+
+  /**
+   * Returns the nonce as a {@link https://github.com/indutny/bn.js/|BN}.
+   */
+  getNonce = (): BN => this.nonceValue.clone();
  
   /**
    * Returns a {@link https://github.com/feross/buffer|Buffer} representation of the [[EVMOutput]].
    */
   toBuffer():Buffer {
-    const bsize: number = this.address.length + this.amount.length + this.assetid.length;
-    const barr: Buffer[] = [this.address, this.amount, this.assetid];
+    const bsize: number = this.address.length + this.amount.length + this.assetid.length + this.nonce.length;
+    const barr: Buffer[] = [this.address, this.amount, this.assetid, this.nonce];
     const buff: Buffer = Buffer.concat(barr, bsize);
     return buff;
   }
 
   /**
-   * Decodes the [[EVMOutput]] as a {@link https://github.com/feross/buffer|Buffer} and returns the size.
+   * Decodes the [[EVMInput]] as a {@link https://github.com/feross/buffer|Buffer} and returns the size.
    */
   fromBuffer(bytes: Buffer, offset: number = 0): number {
     this.address = bintools.copyFrom(bytes, offset, offset + 20);
@@ -48,6 +55,8 @@ export class EVMOutput {
     offset += 8;
     this.assetid = bintools.copyFrom(bytes, offset, offset + 32);
     offset += 32;
+    this.nonce = bintools.copyFrom(bytes, offset, offset + 8);
+    offset += 8;
     return offset;
   }
 
@@ -59,18 +68,21 @@ export class EVMOutput {
   }
 
   /**
-   * An [[EVMOutput]] class which contains address, amount, and assetID.
+   * An [[EVMInput]] class which contains address, amount, and assetID.
    *
    * @param address The address recieving the asset as a {@link https://github.com/feross/buffer|Buffer}
    * @param amount A {@link https://github.com/indutny/bn.js/|BN} representing the locktime
    * @param assetid The asset id which is being sent as a {@link https://github.com/feross/buffer|Buffer}
+   * @param nonce A {@link https://github.com/indutny/bn.js/|BN} representing the nonce 
    */
-  constructor(address: Buffer = undefined, amount: BN = undefined, assetid: Buffer = undefined) {
-    if (typeof address !== 'undefined' && typeof amount !== 'undefined' && typeof assetid !== 'undefined') {
+  constructor(address: Buffer = undefined, amount: BN = undefined, assetid: Buffer = undefined, nonce: BN = undefined) {
+    if (typeof address !== 'undefined' && typeof amount !== 'undefined' && typeof assetid !== 'undefined' && typeof nonce !== 'undefined') {
       this.address = address;
       this.amountValue = amount.clone();
       this.amount = bintools.fromBNToBuffer(amount, 8);
       this.assetid = assetid;
+      this.nonceValue = nonce.clone();
+      this.nonce = bintools.fromBNToBuffer(nonce, 8);
     }
   }
 }  
