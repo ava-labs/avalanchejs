d4331fe2fb7f189b43005c23f30e2a7caa62cfeb
Gabriel Cardona <gabriel@avalabs.org>
2020-12-17 08:08:28 -0800

    Formatting and types.

--- tests/apis/evm/api.test.ts
+++ tests/apis/evm/api.test.ts
@@ -8,25 +8,25 @@ import { Defaults } from 'src/utils/constants';
 /**
  * @ignore
  */
-const bintools = BinTools.getInstance();
+const bintools: BinTools = BinTools.getInstance();
 
 describe('EVMAPI', () => {
-  const networkid:number = 12345;
-  const blockchainid:string = Defaults.network[networkid].C.blockchainID;
-  const ip:string = '127.0.0.1';
-  const port:number = 9650;
-  const protocol:string = 'https';
+  const networkid: number = 12345;
+  const blockchainid: string = Defaults.network[networkid].C.blockchainID;
+  const ip: string = '127.0.0.1';
+  const port: number = 9650;
+  const protocol: string = 'https';
 
-  const username:string = 'AvaLabs';
-  const password:string = 'password';
+  const username: string = 'AvaLabs';
+  const password: string = 'password';
 
-  const avalanche:Avalanche = new Avalanche(ip, port, protocol, networkid, undefined, undefined, undefined, true);
-  let api:EVMAPI;
+  const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkid, undefined, undefined, undefined, true);
+  let api: EVMAPI;
 
 
-  const addrA:string = 'C-' + bech32.encode(avalanche.getHRP(), bech32.toWords(bintools.cb58Decode("B6D4v1VtPYLbiUvYXtW4Px8oE9imC2vGW")));
-  const addrB:string = 'C-' + bech32.encode(avalanche.getHRP(), bech32.toWords(bintools.cb58Decode("P5wdRuZeaDt28eHMP5S3w9ZdoBfo7wuzF")));
-  const addrC:string = 'C-' + bech32.encode(avalanche.getHRP(), bech32.toWords(bintools.cb58Decode("6Y3kysjF9jnHnYkdS9yGAuoHyae2eNmeV")));
+  const addrA: string = 'C-' + bech32.encode(avalanche.getHRP(), bech32.toWords(bintools.cb58Decode("B6D4v1VtPYLbiUvYXtW4Px8oE9imC2vGW")));
+  const addrB: string = 'C-' + bech32.encode(avalanche.getHRP(), bech32.toWords(bintools.cb58Decode("P5wdRuZeaDt28eHMP5S3w9ZdoBfo7wuzF")));
+  const addrC: string = 'C-' + bech32.encode(avalanche.getHRP(), bech32.toWords(bintools.cb58Decode("6Y3kysjF9jnHnYkdS9yGAuoHyae2eNmeV")));
 
   beforeAll(() => {
     api = new EVMAPI(avalanche, '/ext/bc/C/avax', blockchainid);
@@ -37,10 +37,10 @@ describe('EVMAPI', () => {
   });
 
   test('importKey', async () => {
-    const address = addrC;
+    const address: string = addrC;
 
-    const result:Promise<string> = api.importKey(username, password, 'key');
-    const payload:object = {
+    const result: Promise<string> = api.importKey(username, password, 'key');
+    const payload: object = {
       result: {
         address,
       },
@@ -50,17 +50,17 @@ describe('EVMAPI', () => {
     };
 
     mockAxios.mockResponse(responseObj);
-    const response:string = await result;
+    const response: string = await result;
 
     expect(mockAxios.request).toHaveBeenCalledTimes(1);
     expect(response).toBe(address);
   });
 
   test('exportKey', async () => {
-    const key = 'sdfglvlj2h3v45';
+    const key: string = 'sdfglvlj2h3v45';
 
-    const result:Promise<string> = api.exportKey(username, password, addrA);
-    const payload:object = {
+    const result: Promise<string> = api.exportKey(username, password, addrA);
+    const payload: object = {
       result: {
         privateKey: key,
       },
@@ -76,14 +76,60 @@ describe('EVMAPI', () => {
     expect(response).toBe(key);
   });
 
-  test("exportAVAX", async ()=>{
-    let amount = new BN(100);
-    let to = "abcdef";
-    let username = "Robert";
-    let password = "Paulson";
-    let txID = "valid";
-    let result:Promise<string> = api.exportAVAX(username, password, to, amount);
-    let payload:object = {
+  test("exportAVAX", async () =>{
+    let amount: BN = new BN(100);
+    let to: string = "abcdef";
+    let username: string = "Robert";
+    let password: string = "Paulson";
+    let txID: string = "valid";
+    let result: Promise<string> = api.exportAVAX(username, password, to, amount);
+    let payload: object = {
+        "result": {
+            "txID": txID
+        }
+    };
+    let responseObj = {
+        data: payload
+    };
+
+    mockAxios.mockResponse(responseObj);
+    let response: string = await result;
+
+    expect(mockAxios.request).toHaveBeenCalledTimes(1);
+    expect(response).toBe(txID);
+  });
+
+  test("export", async () =>{
+    let amount: BN = new BN(100);
+    let to: string = "abcdef";
+    let assetID: string = "2fombhL7aGPwj3KH4bfrmJwW6PVnMobf9Y2fn9GwxiAAJyFDbe"
+    let username: string = "Robert";
+    let password: string = "Paulson";
+    let txID: string = "valid";
+    let result: Promise<string> = api.export(username, password, to, amount, assetID);
+    let payload: object = {
+        "result": {
+            "txID": txID
+        }
+    };
+    let responseObj = {
+        data: payload
+    };
+
+    mockAxios.mockResponse(responseObj);
+    let response: string = await result;
+
+    expect(mockAxios.request).toHaveBeenCalledTimes(1);
+    expect(response).toBe(txID);
+  });
+
+  test("importAVAX", async () =>{
+    let to: string = "abcdef";
+    let username: string = "Robert";
+    let password: string = "Paulson";
+    let txID: string = "valid";
+    let result: Promise<string> = api.importAVAX(username, password, to, blockchainid);
+    let payload: object = {
         "result": {
             "txID": txID
         }
@@ -99,15 +145,13 @@ describe('EVMAPI', () => {
     expect(response).toBe(txID);
   });
 
-  test("export", async ()=>{
-    let amount = new BN(100);
-    let to = "abcdef";
-    let assetID = "2fombhL7aGPwj3KH4bfrmJwW6PVnMobf9Y2fn9GwxiAAJyFDbe"
-    let username = "Robert";
-    let password = "Paulson";
-    let txID = "valid";
-    let result:Promise<string> = api.export(username, password, to, amount, assetID);
-    let payload:object = {
+  test("import", async () =>{
+    let to: string = "abcdef";
+    let username: string = "Robert";
+    let password: string = "Paulson";
+    let txID: string = "valid";
+    let result: Promise<string> = api.import(username, password, to, blockchainid);
+    let payload: object = {
         "result": {
             "txID": txID
         }
@@ -117,51 +161,7 @@ describe('EVMAPI', () => {
     };
 
     mockAxios.mockResponse(responseObj);
-    let response:string = await result;
-
-    expect(mockAxios.request).toHaveBeenCalledTimes(1);
-    expect(response).toBe(txID);
-  });
-
-  test("importAVAX", async ()=>{
-    let to = "abcdef";
-    let username = "Robert";
-    let password = "Paulson";
-    let txID = "valid";
-    let result:Promise<string> = api.importAVAX(username, password, to, blockchainid);
-    let payload:object = {
-        "result": {
-            "txID": txID
-        }
-    };
-    let responseObj = {
-        data: payload
-    };
-
-    mockAxios.mockResponse(responseObj);
-    let response:string = await result;
-
-    expect(mockAxios.request).toHaveBeenCalledTimes(1);
-    expect(response).toBe(txID);
-  });
-
-  test("import", async ()=>{
-    let to = "abcdef";
-    let username = "Robert";
-    let password = "Paulson";
-    let txID = "valid";
-    let result:Promise<string> = api.import(username, password, to, blockchainid);
-    let payload:object = {
-        "result": {
-            "txID": txID
-        }
-    };
-    let responseObj = {
-        data: payload
-    };
-
-    mockAxios.mockResponse(responseObj);
-    let response:string = await result;
+    let response: string = await result;
 
     expect(mockAxios.request).toHaveBeenCalledTimes(1);
     expect(response).toBe(txID);
