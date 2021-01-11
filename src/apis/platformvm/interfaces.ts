9520fad42f395819bb1038c0b912b2f247f72309
Gabriel Cardona <gabriel@avalabs.org>
2021-01-11 05:18:53 -0800

    Add iGetMinStakeResponse interface.

--- src/apis/platformvm/interfaces.ts
+++ src/apis/platformvm/interfaces.ts
@@ -5,19 +5,25 @@
 
 import { iUTXOID, UTXOResponse } from "./../../common/interfaces";
 import { UTXOSet } from "./../../apis/platformvm";
+import BN from "bn.js";
 
 export interface iPlatformVMUTXOResponse extends UTXOResponse {
-  utxos: UTXOSet
+  utxos:UTXOSet
 }
 
 export interface iGetBalanceResponse {
   balance:string
-  unlocked: string
-  lockedStakeable: string
-  lockedNotStakeable: string
+  unlocked:string
+  lockedStakeable:string
+  lockedNotStakeable:string
   utxoIDs:iUTXOID[] 
 }
 
 export interface iGetBalanceParams {
   address:string
+}
+
+export interface iGetMinStakeResponse {
+  minValidatorStake:BN
+  minDelegatorStake:BN
 }
