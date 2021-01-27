ccd8880db8105dcdf86cfe0d6548960f03e4c983
Gabriel Cardona <gabriel@avalabs.org>
2021-01-27 02:56:36 -0800

    AVM example scripts.

---
+++ examples/avm/README.md
@@ -0,0 +1,14 @@
+# AVM RPC
+
+Tests for the Avalanche [AVM RPC](https://docs.avax.network/build/avalanchego-apis/exchange-chain-x-chain-api)
+
+* [baseTx-ant.ts](./baseTx-ant.ts)
+* [baseTx-avax-create-multisig.ts](./baseTx-avax-create-multisig.ts)
+* [baseTx-avax-send-multisig.ts](./baseTx-avax-send-multisig.ts)
+* [baseTx-avax.ts](./baseTx-avax.ts)
+* [createAssetTx-ant.ts](./createAssetTx-ant.ts)
+* [createAssetTx-nft.ts](./createAssetTx-nft.ts)
+* [exportTx-ant-cchain.ts](./exportTx-ant-cchain.ts)
+* [exportTx-avax-cchain.ts](./exportTx-avax-cchain.ts)
+* [importTx-ant-cchain.ts](./importTx-ant-cchain.ts)
+* [importTx-avax-cchain.ts](./importTx-avax-cchain.ts)
---
+++ examples/avm/baseTx-avax-create-multisig.ts
@@ -0,0 +1,96 @@
+import { 
+  Avalanche,
+  BinTools,
+  BN,
+  Buffer
+} from "../../src";
+import {
+  AVMAPI, 
+  KeyChain as AVMKeyChain,
+  SECPTransferOutput,
+  SECPTransferInput,
+  TransferableOutput,
+  TransferableInput,
+  UTXOSet,
+  UTXO,
+  AmountOutput,
+  UnsignedTx,
+  Tx,
+  BaseTx
+} from "../../src/apis/avm"
+import { Defaults } from "../../src/utils"
+      
+const ip: string = "localhost"
+const port: number = 9650
+const protocol: string = "http"
+const networkID: number = 12345
+const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
+const xchain: AVMAPI = avalanche.XChain()
+const bintools: BinTools = BinTools.getInstance()
+const xKeychain: AVMKeyChain = xchain.keyChain()
+let privKey: string = "PrivateKey-ewoqjP7PxY4yr3iLTpLisriqt94hdyDFNgchSxGGztUrTXtNN"
+// X-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u
+xKeychain.importKey(privKey)
+privKey = "PrivateKey-R6e8f5QSa89DjpvL9asNdhdJ4u8VqzMJStPV8VVdDmLgPd8a4"
+// X-local15s7p7mkdev0uajrd0pzxh88kr8ryccztek57g0
+xKeychain.importKey(privKey)
+privKey = "PrivateKey-24b2s6EqkBp9bFG5S3Xxi4bjdxFqeRk56ck7QdQArVbwKkAvxz"
+// X-local1aekly2mwnsz6lswd6u0jqvd9u6yddt58duwcc9
+xKeychain.importKey(privKey)
+const xAddresses: Buffer[] = xchain.keyChain().getAddresses()
+  
+const xAddressStrings: string[] = xchain.keyChain().getAddressStrings()
+const blockchainid: string = Defaults.network['12345'].X.blockchainID
+const outputs: TransferableOutput[] = []
+const inputs: TransferableInput[] = []
+const fee: BN = xchain.getDefaultTxFee()
+const threshold: number = 3
+const locktime: BN = new BN(0)
+const memo: Buffer = bintools.stringToBuffer("AVM manual create multisig BaseTx to send AVAX")
+// Uncomment for codecID 00 01
+// const codecID: number = 1
+      
+const main = async (): Promise<any> => {
+  const avaxAssetID: Buffer = await xchain.getAVAXAssetID()
+  const getBalanceResponse: any = await xchain.getBalance(xAddressStrings[0], bintools.cb58Encode(avaxAssetID))
+  const balance: BN = new BN(getBalanceResponse['balance'])
+  const secpTransferOutput: SECPTransferOutput = new SECPTransferOutput(balance.sub(fee), xAddresses, locktime, threshold)
+  // Uncomment for codecID 00 01
+//   secpTransferOutput.setCodecID(codecID)
+  const transferableOutput: TransferableOutput = new TransferableOutput(avaxAssetID, secpTransferOutput)
+  outputs.push(transferableOutput)
+  
+  const avmUTXOResponse: any= await xchain.getUTXOs(xAddressStrings)
+  const utxoSet: UTXOSet = avmUTXOResponse.utxos
+  const utxos: UTXO[] = utxoSet.getAllUTXOs()
+  utxos.forEach((utxo: UTXO) => {
+    const amountOutput: AmountOutput = utxo.getOutput() as AmountOutput
+    const amt: BN = amountOutput.getAmount().clone()
+    const txid: Buffer = utxo.getTxID()
+    const outputidx: Buffer = utxo.getOutputIdx()
+  
+    const secpTransferInput: SECPTransferInput = new SECPTransferInput(amt)
+    // Uncomment for codecID 00 01
+    // secpTransferInput.setCodecID(codecID)
+    secpTransferInput.addSignatureIdx(0, xAddresses[0])
+  
+    const input: TransferableInput = new TransferableInput(txid, outputidx, avaxAssetID, secpTransferInput)
+    inputs.push(input)
+  })
+  
+  const baseTx: BaseTx = new BaseTx (
+    networkID,
+    bintools.cb58Decode(blockchainid),
+    outputs,
+    inputs,
+    memo
+  )
+  // Uncomment for codecID 00 01
+//   baseTx.setCodecID(codecID)
+  const unsignedTx: UnsignedTx = new UnsignedTx(baseTx)
+  const tx: Tx = unsignedTx.sign(xKeychain)
+  const id: string = await xchain.issueTx(tx)
+  console.log(id)
+} 
+main()
+    
