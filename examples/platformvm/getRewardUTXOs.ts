9ff62748b8aca5ba6ea15c5594843e05dfaf32b8
Gabriel Cardona <gabriel@avalabs.org>
2021-05-28 09:55:48 -0700

    Rollback

--- examples/platformvm/getRewardUTXOs.ts
+++ examples/platformvm/getRewardUTXOs.ts
@@ -1,10 +1,16 @@
-import { Avalanche } from "../../src"
-import { GetRewardUTXOsResponse } from "../../src/common"
-import { PlatformVMAPI } from "../../src/apis/platformvm"
+import {
+  Avalanche
+} from "../../dist"
+import { GetRewardUTXOsResponse } from "../../dist/common"
+import {
+  PlatformVMAPI,
+  KeyChain,
+  KeyPair
+} from "../../dist/apis/platformvm"
 
-const ip: string = "localhost"
+const ip: string = 'localhost'
 const port: number = 9650
-const protocol: string = "http"
+const protocol: string = 'http'
 const networkID: number = 12345
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const pchain: PlatformVMAPI = avalanche.PChain()
