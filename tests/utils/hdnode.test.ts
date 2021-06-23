132f59318d499f94a4ca2d36779b932ed22f4b8c
Gabriel Cardona <gabriel@avalabs.org>
2021-06-22 18:17:00 -0700

    Reformat tests.

--- tests/utils/hdnode.test.ts
+++ tests/utils/hdnode.test.ts
@@ -15,7 +15,9 @@ describe("HDNode", (): void => {
   const xPriv: string = "xprv9s21ZrQH143K4RH1nRkHwuVz3qGREBLobwUoUBowLDucQXm4do8jvz12agvjHrAwjJXtq9BZ87WBPUPScDBnjKvBKVQ5xbS7GQwJKW7vXLD"
   const childXPriv: string = "xprvA7X7udsZk3q9mNMcGnN8PKHv5eHm6JA3TRzW2HsWnrYHbccXh5YMnRLA83VCPKWQUFmKf9AfCXSmoFs7HJ8Yr1LK52wJDVk262vGFszM4nb"
   const xPub: string = "xpub661MyMwAqRbcFSdAk5S6UECmA6MFQWiRBfPU5AsVcmrKY5HoFKPNYrKEq7isvaZVfNxhkrv5oXxFpQc6AVEcVW5NxeamKD6LyLUDMntbnq7"
+  const seed: string = 'a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af'
   const msg: string = "bb413645935a9bf1ecf0c3d30df2d573"
+  const m: string = "immune year obscure laptop wage diamond join glue ecology envelope box fade mixed cradle athlete absorb stick rival punch dinosaur skin blind benefit pretty"
   const addrs: string[] = [
     'X-avax15qwuklmrfcmfw78yvka9pjsukjeevl4aveehq0',
     'X-avax13wqaxm6zgjq5qwzuyyxyl9yrz3edcgwgfht6gt',
@@ -30,20 +32,18 @@ describe("HDNode", (): void => {
   ]
 
   test("derive", (): void => {
-    const seed: string = 'a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af'
     const hdnode: HDNode = new HDNode(seed)
-    const child = hdnode.derive("m/9000'/2614666'/4849181'/4660'/2'/1/3")
+    const path: string = "m/9000'/2614666'/4849181'/4660'/2'/1/3"
+    const child = hdnode.derive(path)
     expect(child.privateExtendedKey).toBe(childXPriv)
   })
 
   test("fromMasterSeedBuffer", (): void => {
-    const seed: string = 'a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af'
     const hdnode: HDNode = new HDNode(Buffer.from(seed))
     expect(hdnode.privateExtendedKey).toBe(xPriv)
   })
 
   test("fromMasterSeedString", (): void => {
-    const seed: string = 'a0c42a9c3ac6abf2ba6a9946ae83af18f51bf1c9fa7dacc4c92513cc4dd015834341c775dcd4c0fac73547c5662d81a9e9361a0aac604a73a321bd9103bce8af'
     const hdnode: HDNode = new HDNode(seed)
     expect(hdnode.privateExtendedKey).toBe(xPriv)
   })
@@ -78,7 +78,6 @@ describe("HDNode", (): void => {
   })
 
   test("BIP44", (): void => {
-    const m: string = "immune year obscure laptop wage diamond join glue ecology envelope box fade mixed cradle athlete absorb stick rival punch dinosaur skin blind benefit pretty"
     const seed: Buffer = mnemonic.mnemonicToSeedSync(m)
     const hdnode: HDNode = new HDNode(seed)
     for (let i: number = 0; i <= 9; i++) {
