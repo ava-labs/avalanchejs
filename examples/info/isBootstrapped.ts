cce00d35b3bc6a0613d6247dbce019abfc99fc1f
Gabriel Cardona <gabriel@avalabs.org>
2021-07-21 08:06:03 -0700

    Testing

--- examples/info/isBootstrapped.ts
+++ examples/info/isBootstrapped.ts
@@ -3,7 +3,7 @@ import { InfoAPI } from "../../dist/apis/info"
 
 const ip: string = 'localhost'
 const port: number = 9650
-const protocol: string = "http"
+const protocol: string = 'http'
 const networkID: number = 12345
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const info: InfoAPI = avalanche.Info()
