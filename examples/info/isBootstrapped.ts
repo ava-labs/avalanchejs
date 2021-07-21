50df04957f8fdd38b9d419063d54871fdf0c4f9f
Gabriel Cardona <gabriel@avalabs.org>
2021-07-21 07:58:15 -0700

    Testing lint.

--- examples/info/isBootstrapped.ts
+++ examples/info/isBootstrapped.ts
@@ -1,7 +1,7 @@
 import { Avalanche } from "../../dist"
 import { InfoAPI } from "../../dist/apis/info"
 
-const ip: string = "localhost"
+const ip: string = 'localhost'
 const port: number = 9650
 const protocol: string = "http"
 const networkID: number = 12345
