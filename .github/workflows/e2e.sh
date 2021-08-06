44d426cb7b019653c67d088ec34423bf9b48b99e
Felipe Madero <felipe.madero@gmail.com>
2021-08-06 00:44:32 -0300

    try e2e compiling avalanchego in container

--- .github/workflows/e2e.sh
+++ .github/workflows/e2e.sh
@@ -1,1 +1,7 @@
-docker run -it avaplatform/avalanche-testing:chaos-4 sh
+mount_spec=/home/runner/work/avalanchejs/avalanchejs/:/avalanchejs/
+image=avaplatform/avalanche-testing:chaos-4 
+
+docker run -it -v $mount_spec $image \
+    scripts/executions/local-e2e-tests.sh --jsdir=/avalanchejs/ --tests="\"AvalancheJS\"" --runlocal=./local-e2e-tests.bin \
+    --branch=master --repo=public
+
