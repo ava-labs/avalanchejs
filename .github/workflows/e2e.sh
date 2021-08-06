54fcb779c1201074e197caaded9a9d2d8780495f
Felipe Madero <felipe.madero@gmail.com>
2021-08-06 00:48:40 -0300

    try 3

--- .github/workflows/e2e.sh
+++ .github/workflows/e2e.sh
@@ -1,7 +1,6 @@
 mount_spec=/home/runner/work/avalanchejs/avalanchejs/:/avalanchejs/
 image=avaplatform/avalanche-testing:chaos-4 
 
-docker run -it -v $mount_spec $image \
+docker run -v $mount_spec $image \
     scripts/executions/local-e2e-tests.sh --jsdir=/avalanchejs/ --tests="\"AvalancheJS\"" --runlocal=./local-e2e-tests.bin \
     --branch=master --repo=public
-
