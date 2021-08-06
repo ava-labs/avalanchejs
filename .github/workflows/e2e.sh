faa41809e5677012a38bd6d8d351e8dab4f6d8c4
Felipe Madero <felipe.madero@gmail.com>
2021-08-06 01:09:41 -0300

    fix location of runlocal

--- .github/workflows/e2e.sh
+++ .github/workflows/e2e.sh
@@ -4,5 +4,5 @@ image=avaplatform/avalanche-testing:chaos-4
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
 docker run -v $mount_spec $image \
-    scripts/executions/local-e2e-tests.sh --jsdir=/avalanchejs/ --tests="\"AvalancheJS\"" --runlocal=./local-e2e-tests.bin \
+    scripts/executions/local-e2e-tests.sh --jsdir=/avalanchejs/ --tests="\"AvalancheJS\"" --runlocal=/run/local-e2e-tests.bin \
     --branch=master --repo=public
