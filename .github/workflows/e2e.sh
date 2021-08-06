d7fa55f41b4afff8fdd8e6dbcbd77d2b4b9e3df8
Felipe Madero <felipe.madero@gmail.com>
2021-08-06 01:06:31 -0300

    add docker login

--- .github/workflows/e2e.sh
+++ .github/workflows/e2e.sh
@@ -1,6 +1,8 @@
 mount_spec=/home/runner/work/avalanchejs/avalanchejs/:/avalanchejs/
 image=avaplatform/avalanche-testing:chaos-4 
 
+echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
+
 docker run -v $mount_spec $image \
     scripts/executions/local-e2e-tests.sh --jsdir=/avalanchejs/ --tests="\"AvalancheJS\"" --runlocal=./local-e2e-tests.bin \
     --branch=master --repo=public
