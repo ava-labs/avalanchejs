086c3b0247aaf217bc26c05d59c37679e8ede26e
paOol <paoolkim@gmail.com>
2021-09-01 14:33:20 -0700

    add operationTx comments

--- examples/avm/operationTx-mint-ant.ts
+++ examples/avm/operationTx-mint-ant.ts
@@ -23,6 +23,8 @@ import {
   Defaults
 } from "../../src/utils"
 
+// before you run this example buildCreateNFTAssetTx.ts
+
 const getUTXOIDs = (
   utxoSet: UTXOSet,
   txid: string,
@@ -183,6 +185,8 @@ const main = async (): Promise<any> => {
     memo,
     operations
   )
+  console.log("operationTx", operationTx)
+
   // Uncomment for codecID 00 01
   // operationTx.setCodecID(codecID)
   const unsignedTx: UnsignedTx = new UnsignedTx(operationTx)
