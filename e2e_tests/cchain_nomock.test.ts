7d9032cf41fb9fa95a0ce401f11dc406dcb7e64b
Felipe Madero <felipe.madero@gmail.com>
2021-09-20 15:12:56 -0300

    fix test to snowmanpp and fix minor issue

--- e2e_tests/cchain_nomock.test.ts
+++ e2e_tests/cchain_nomock.test.ts
@@ -9,7 +9,7 @@ describe("CChain", (): void => {
   // test_name        response_promise                            resp_fn          matcher           expected_value/obtained_value
   const tests_spec: any = [
     ["getBaseFee", () => cchain.getBaseFee(), (x) => x, Matcher.toBe, () => "0x34630b8a00"],
-    ["getMaxPriorityFeePerGas", () => cchain.getMaxPriorityFeePerGas(), (x) => x, Matcher.toBe, () => "0x2540be400"],
+    ["getMaxPriorityFeePerGas", () => cchain.getMaxPriorityFeePerGas(), (x) => x, Matcher.toBe, () => "0x0"],
   ]
 
   createTests(tests_spec)
