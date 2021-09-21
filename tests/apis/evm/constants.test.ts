6519a0df2dc9939dcad3d633d3f4a1a3e65e294a
Gabriel Cardona <gabriel@avalabs.org>
2021-09-21 00:01:21 -0700

    Test equality, even number and positive number.

--- tests/apis/evm/constants.test.ts
+++ tests/apis/evm/constants.test.ts
@@ -5,20 +5,28 @@ import { Networks, Network } from "../../../src/apis/evm/constants"
 describe("EVMConstants", (): void => {
   describe("compare min and max gas prices", (): void => {
     const networks: Networks = Defaults.network
-    const min: number = 25000000000
-    const max: number = 1000000000000
+    const minNum: number = 25000000000
+    const maxNum: number = 1000000000000
     const networkIDs: number[] = [1, 5, 12345]
     networkIDs.forEach((networkID: number): void => {
       test(`NetworkID: ${networkID}`, async (): Promise<void> => {
         const localNetwork: Network = networks[networkID]
+
         const minGasPriceBN: BN = localNetwork.C.minGasPrice
-        const maxGasPriceBN: BN = localNetwork.C.maxGasPrice
         const minGasPriceNum: number = new BN(minGasPriceBN).toNumber()
+
+        const maxGasPriceBN: BN = localNetwork.C.maxGasPrice
         const maxGasPriceNum: number = new BN(maxGasPriceBN).toNumber()
+
         expect(minGasPriceNum).toBeLessThanOrEqual(maxGasPriceNum)
-        expect(minGasPriceNum).toEqual(min)
+        expect(minGasPriceNum).toEqual(minNum)
+        expect(minGasPriceBN.isEven()).toBeTruthy()
+        expect(minGasPriceBN.isNeg()).toBeFalsy()
+
         expect(maxGasPriceNum).toBeGreaterThanOrEqual(minGasPriceNum)
-        expect(maxGasPriceNum).toEqual(max)
+        expect(maxGasPriceNum).toEqual(maxNum)
+        expect(maxGasPriceBN.isEven()).toBeTruthy()
+        expect(maxGasPriceBN.isNeg()).toBeFalsy()
       })
     })
   })
