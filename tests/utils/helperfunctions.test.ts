cda4c14523b7bbcecb3f9c64cf0d773202c441dc
Gabriel Cardona <gabriel@avalabs.org>
2021-11-24 14:07:40 -0800

    Tests to confirm that the byteCost should always be 1 nAVAX per byte

--- tests/utils/helperfunctions.test.ts
+++ tests/utils/helperfunctions.test.ts
@@ -5,8 +5,19 @@ describe("HelperFunctions", (): void => {
   test("calcBytesCost", (): void => {
     const importTx: ImportTx = new ImportTx()
     // an empty EVM ImportTx is 76 bytes
-    const size: number = 76
-    const cost: number = calcBytesCost(importTx.toBuffer().byteLength)
-    expect(cost).toEqual(size)
+    let cost: number = 76
+    let bytesCost: number = calcBytesCost(importTx.toBuffer().byteLength)
+    expect(bytesCost).toEqual(cost)
+
+    // the byteCost should always be 1 nAVAX per byte
+    let size: number = 100
+    cost = 100
+    bytesCost = calcBytesCost(size)
+    expect(bytesCost).toEqual(cost)
+
+    size = 507
+    cost = 507
+    bytesCost = calcBytesCost(size)
+    expect(bytesCost).toEqual(cost)
   })
 })
