1a3ba9fc48865eacd372f0812e35164e6f299f76
Gabriel Cardona <gabriel@avalabs.org>
2021-11-29 20:08:32 -0800

    Formatting

--- tests/utils/helperfunctions.test.ts
+++ tests/utils/helperfunctions.test.ts
@@ -21,16 +21,18 @@ describe("HelperFunctions", (): void => {
     bytesCost = calcBytesCost(size)
     expect(cost).toEqual(bytesCost)
 
-    let hex: string = "000030399d0775f450604bd2fbc49ce0c5c1c6dfeb2dc2acb8c92c26eeae6e6df4502b19d891ad56056d9c01f18f43f58b5c784ad07a4a49cf3d1f11623804b5cba2c6bf000000018db97c7cece249c2b98bdc0226cc4c2a57bf52fc00b1a2bc2ec50000dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db000000000000000000000001dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db0000000700b1a2bc2eb5bdc0000000000000000000000001000000013cb7d3842e8cee6a0ebd09f1fe884f6861e1b29c"
+    let hex: string =
+      "000030399d0775f450604bd2fbc49ce0c5c1c6dfeb2dc2acb8c92c26eeae6e6df4502b19d891ad56056d9c01f18f43f58b5c784ad07a4a49cf3d1f11623804b5cba2c6bf000000018db97c7cece249c2b98bdc0226cc4c2a57bf52fc00b1a2bc2ec50000dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db000000000000000000000001dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db0000000700b1a2bc2eb5bdc0000000000000000000000001000000013cb7d3842e8cee6a0ebd09f1fe884f6861e1b29c"
     const exportTx: ExportTx = new ExportTx()
     let unsignedTx: UnsignedTx = new UnsignedTx(exportTx)
-    exportTx.fromBuffer(new Buffer(hex, 'hex'))
+    exportTx.fromBuffer(new Buffer(hex, "hex"))
     cost = costExportTx(unsignedTx)
     bytesCost = 11230
     expect(cost).toEqual(bytesCost)
 
-    hex = "000030399d0775f450604bd2fbc49ce0c5c1c6dfeb2dc2acb8c92c26eeae6e6df4502b19d891ad56056d9c01f18f43f58b5c784ad07a4a49cf3d1f11623804b5cba2c6bf0000000000000000"
-    importTx.fromBuffer(new Buffer(hex, 'hex'))
+    hex =
+      "000030399d0775f450604bd2fbc49ce0c5c1c6dfeb2dc2acb8c92c26eeae6e6df4502b19d891ad56056d9c01f18f43f58b5c784ad07a4a49cf3d1f11623804b5cba2c6bf0000000000000000"
+    importTx.fromBuffer(new Buffer(hex, "hex"))
     unsignedTx = new UnsignedTx(importTx)
     cost = costImportTx(unsignedTx)
     bytesCost = 10082
@@ -38,10 +40,11 @@ describe("HelperFunctions", (): void => {
   })
 
   test("getCost", (): void => {
-    const txHex: string = "0x000000000000000030399d0775f450604bd2fbc49ce0c5c1c6dfeb2dc2acb8c92c26eeae6e6df4502b19d891ad56056d9c01f18f43f58b5c784ad07a4a49cf3d1f11623804b5cba2c6bf000000012b845c6ab4405c7e379971199cde5367fe7591cbe6e3e81bd5925cea9afa859e00000000dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db00000005007ba5886cf8f1400000000100000000000000018db97c7cece249c2b98bdc0226cc4c2a57bf52fc007aa63138f3a540dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db"
-    console.log(new Buffer(txHex).toString('hex'))
+    const txHex: string =
+      "0x000000000000000030399d0775f450604bd2fbc49ce0c5c1c6dfeb2dc2acb8c92c26eeae6e6df4502b19d891ad56056d9c01f18f43f58b5c784ad07a4a49cf3d1f11623804b5cba2c6bf000000012b845c6ab4405c7e379971199cde5367fe7591cbe6e3e81bd5925cea9afa859e00000000dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db00000005007ba5886cf8f1400000000100000000000000018db97c7cece249c2b98bdc0226cc4c2a57bf52fc007aa63138f3a540dbcf890f77f49b96857648b72b77f9f82937f28a68704af05da0dc12ba53f2db"
+    console.log(new Buffer(txHex).toString("hex"))
     const unsignedTx: UnsignedTx = new UnsignedTx()
-    unsignedTx.fromBuffer(new Buffer(txHex, 'hex'))
+    unsignedTx.fromBuffer(new Buffer(txHex, "hex"))
     console.log(unsignedTx)
     // TODO - flesh out these tests
     // const importTx: ImportTx = new ImportTx()
