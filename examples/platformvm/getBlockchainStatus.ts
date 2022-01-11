7b8dfe5f4e8c78637c9f61dec302b762b8b558c3
Gabriel Cardona <gabriel@avalabs.org>
2022-01-11 11:21:16 -0800

    Add RPC example scripts:
    * getBlockchainStatus.ts
    * getBlockchains.ts
    * getSubnets.ts

---
+++ examples/platformvm/getBlockchainStatus.ts
@@ -0,0 +1,17 @@
+import { Avalanche } from "../../src"
+import { PlatformVMAPI } from "../../src/apis/platformvm"
+
+const ip: string = "localhost"
+const port: number = 9650
+const protocol: string = "http"
+const networkID: number = 12345
+const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
+const pchain: PlatformVMAPI = avalanche.PChain()
+
+const main = async (): Promise<any> => {
+  const blockchainID: string = "2AymB4Mb6mErFNsDB8aWb77Ui8oyogXgDyRe9RVQBtqfXzKoUc"
+  const blockchainStatus: string = await pchain.getBlockchainStatus(blockchainID)
+  console.log(blockchainStatus)
+}
+
+main()
