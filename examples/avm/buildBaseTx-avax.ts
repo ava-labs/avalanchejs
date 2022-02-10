1915f72393b322f5f0f373b437273e6cc05f0a48
Gabriel Cardona <gabriel@avalabs.org>
2022-02-09 19:14:52 -0800

    AVAX BaseTx on a custom network via buildBaseTx utility method.

--- examples/avm/buildBaseTx-avax.ts
+++ examples/avm/buildBaseTx-avax.ts
@@ -1,29 +1,39 @@
 import { Avalanche, BN, Buffer, BinTools } from "../../src"
 import { AVMAPI, KeyChain, UTXOSet, UnsignedTx, Tx } from "../../src/apis/avm"
-import {} from "../../src/utils"
+import { MILLIAVAX } from "../../src/utils"
 import {
   PrivateKeyPrefix,
   DefaultLocalGenesisPrivateKey,
   UnixNow,
-  Defaults
 } from "../../src/utils"
 
+const bintools: BinTools = BinTools.getInstance()
 const ip: string = "localhost"
 const port: number = 9650
 const protocol: string = "http"
-const networkID: number = 12345
-const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
+const networkID: number = 1337
+const xBlockchainID: string =
+  "qzfF3A11KzpcHkkqznEyQgupQrCNS6WV6fTUTwZpEKqhj1QE7"
+const xBlockchainIDBuf: Buffer = bintools.cb58Decode(xBlockchainID)
+const avaxAssetID: string = "BUuypiq2wyuLMvyhzFXcPyxPMCgSp7eeDohhQRqTChoBjKziC"
+const avaxAssetIDBuf: Buffer = bintools.cb58Decode(avaxAssetID)
+const avalanche: Avalanche = new Avalanche(
+  ip,
+  port,
+  protocol,
+  networkID,
+  xBlockchainID
+)
 const xchain: AVMAPI = avalanche.XChain()
 const xKeychain: KeyChain = xchain.keyChain()
 const privKey: string = `${PrivateKeyPrefix}${DefaultLocalGenesisPrivateKey}`
 xKeychain.importKey(privKey)
 const xAddressStrings: string[] = xchain.keyChain().getAddressStrings()
-const avaxAssetID: string = Defaults.network[networkID].X["avaxAssetID"]
 const asOf: BN = UnixNow()
 const threshold: number = 1
 const locktime: BN = new BN(0)
 const memo: Buffer = Buffer.from("AVM utility method buildBaseTx to send AVAX")
-const fee: BN = xchain.getDefaultTxFee()
+const fee: BN = MILLIAVAX
 
 const main = async (): Promise<any> => {
   const getBalanceResponse: any = await xchain.getBalance(
@@ -33,10 +43,11 @@ const main = async (): Promise<any> => {
   const balance: BN = new BN(getBalanceResponse.balance)
   const avmUTXOResponse: any = await xchain.getUTXOs(xAddressStrings)
   const utxoSet: UTXOSet = avmUTXOResponse.utxos
+  const amount: BN = balance.sub(fee) 
 
   const unsignedTx: UnsignedTx = await xchain.buildBaseTx(
     utxoSet,
-    balance.sub(fee),
+    amount,
     avaxAssetID,
     xAddressStrings,
     xAddressStrings,
