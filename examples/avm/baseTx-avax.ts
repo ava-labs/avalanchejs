c8230362245e64225635d551a579d8f07e864b61
Gabriel Cardona <gabriel@avalabs.org>
2022-02-09 19:09:37 -0800

    Manually build BaseTx with custom network.

--- examples/avm/baseTx-avax.ts
+++ examples/avm/baseTx-avax.ts
@@ -14,29 +14,37 @@ import {
   BaseTx
 } from "../../src/apis/avm"
 import {
-  Defaults,
   PrivateKeyPrefix,
-  DefaultLocalGenesisPrivateKey
+  DefaultLocalGenesisPrivateKey,
+  MILLIAVAX
 } from "../../src/utils"
 
+const bintools: BinTools = BinTools.getInstance()
 const ip: string = "localhost"
 const port: number = 9650
 const protocol: string = "http"
-const networkID: number = 12345
-const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
+const networkID: number = 1337
+const xBlockchainID: string =
+  "qzfF3A11KzpcHkkqznEyQgupQrCNS6WV6fTUTwZpEKqhj1QE7"
+const xBlockchainIDBuf: Buffer = bintools.cb58Decode(xBlockchainID)
+const avaxAssetID: string = "BUuypiq2wyuLMvyhzFXcPyxPMCgSp7eeDohhQRqTChoBjKziC"
+const avaxAssetIDBuf: Buffer = bintools.cb58Decode(avaxAssetID)
+const avalanche: Avalanche = new Avalanche(
+  ip,
+  port,
+  protocol,
+  networkID,
+  xBlockchainID
+)
 const xchain: AVMAPI = avalanche.XChain()
-const bintools: BinTools = BinTools.getInstance()
 const xKeychain: KeyChain = xchain.keyChain()
 const privKey: string = `${PrivateKeyPrefix}${DefaultLocalGenesisPrivateKey}`
 xKeychain.importKey(privKey)
 const xAddresses: Buffer[] = xchain.keyChain().getAddresses()
 const xAddressStrings: string[] = xchain.keyChain().getAddressStrings()
-const blockchainID: string = Defaults.network[networkID].X.blockchainID
-const avaxAssetID: string = Defaults.network[networkID].X.avaxAssetID
-const avaxAssetIDBuf: Buffer = bintools.cb58Decode(avaxAssetID)
 const outputs: TransferableOutput[] = []
 const inputs: TransferableInput[] = []
-const fee: BN = xchain.getDefaultTxFee()
+const fee: BN = MILLIAVAX
 const threshold: number = 1
 const locktime: BN = new BN(0)
 const memo: Buffer = Buffer.from("AVM manual BaseTx to send AVAX")
@@ -88,7 +96,7 @@ const main = async (): Promise<any> => {
 
   const baseTx: BaseTx = new BaseTx(
     networkID,
-    bintools.cb58Decode(blockchainID),
+    xBlockchainIDBuf,
     outputs,
     inputs,
     memo
