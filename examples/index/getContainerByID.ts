17052959c9c16293a73443941b85f7752dee9011
Gabriel Cardona <gabriel@avalabs.org>
2022-03-09 10:05:08 -0800

    Health and index examples ported to working w/ the anr.

--- examples/index/getContainerByID.ts
+++ examples/index/getContainerByID.ts
@@ -5,13 +5,13 @@ import { GetContainerByIDResponse } from "../../src/apis/index/interfaces"
 const ip: string = "localhost"
 const port: number = 9650
 const protocol: string = "http"
-const networkID: number = 12345
+const networkID: number = 1337
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const index: IndexAPI = avalanche.Index()
 
 const main = async (): Promise<any> => {
   const containerID: string =
-    "GwNpFGDWwzBxiq2Goqr3FqTyT3toyGr7LMCUG3tm72wt39xDZ"
+    "eLXEKFFMgGmK7ZLokCFjppdBfGy5hDuRqh5uJVyXXPaRErpAX"
   const encoding: string = "hex"
   const baseurl: string = "/ext/index/X/tx"
   const containerByIndex: GetContainerByIDResponse =
--- examples/index/getLastAccepted.ts
+++ examples/index/getLastAccepted.ts
@@ -5,7 +5,7 @@ import { GetLastAcceptedResponse } from "../../src/apis/index/interfaces"
 const ip: string = "localhost"
 const port: number = 9650
 const protocol: string = "http"
-const networkID: number = 12345
+const networkID: number = 1337
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const index: IndexAPI = avalanche.Index()
 
