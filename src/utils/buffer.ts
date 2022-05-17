05863b96bd5ec7d992e8ae51936c3df035db00c0
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2022-05-17 03:26:15 -0400

    import nit

--- src/utils/buffer.ts
+++ src/utils/buffer.ts
@@ -1,7 +1,7 @@
-import * as buffer from 'worktop/buffer';
+import {toHEX} from 'worktop/buffer';
 
 export function bufferToNumber(buf: Uint8Array) {
-  return Number.parseInt(buffer.toHEX(buf), 16);
+  return Number.parseInt(toHEX(buf), 16);
 }
 
 export * as buffer from 'worktop/buffer';
