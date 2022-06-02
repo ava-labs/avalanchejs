caf014ec216fe1a4f985683ecd5253958239831b
rajranjan0608 <rranjan01234@gmail.com>
2022-06-02 16:44:27 +0530

    code refactor

--- examples/index/getIndex.ts
+++ examples/index/getIndex.ts
@@ -9,15 +9,10 @@ const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const index: IndexAPI = avalanche.Index()
 
 const main = async (): Promise<any> => {
-  const id: string =
-    "eLXEKFFMgGmK7ZLokCFjppdBfGy5hDuRqh5uJVyXXPaRErpAX"
+  const id: string = "eLXEKFFMgGmK7ZLokCFjppdBfGy5hDuRqh5uJVyXXPaRErpAX"
   const encoding: string = "hex"
   const baseurl: string = "/ext/index/X/tx"
-  const containerIndex: string = await index.getIndex(
-    id,
-    encoding,
-    baseurl
-  )
+  const containerIndex: string = await index.getIndex(id, encoding, baseurl)
   console.log(containerIndex)
 }
 
