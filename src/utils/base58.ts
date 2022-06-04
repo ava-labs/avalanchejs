36e2d7a3afb830f4705c85766b00d44f52ee01dc
Dhruba Basu <7675102+dhrubabasu@users.noreply.github.com>
2022-06-04 16:47:07 -0700

    add merge util to buffer lib (#8)

--- src/utils/base58.ts
+++ src/utils/base58.ts
@@ -1,14 +1,11 @@
 import {base58} from '@scure/base';
 import type {BytesCoder} from '@scure/base';
 import {sha256} from '@noble/hashes/sha256';
+import {merge} from './buffer';
 
 export const base58check: BytesCoder = {
   encode(data) {
-    const merged = new Uint8Array(data.length + 4);
-    merged.set(data);
-    merged.set(sha256(data).subarray(-4), data.length);
-
-    return base58.encode(merged);
+    return base58.encode(merge(data, sha256(data).subarray(-4)));
   },
   decode(string) {
     return base58.decode(string).subarray(0, -4);
--- src/utils/buffer.ts
+++ src/utils/buffer.ts
@@ -8,4 +8,11 @@ export function bufferToNumber(buf: Uint8Array) {
   return Number.parseInt(toHEX(buf), 16);
 }
 
+export function merge(buf1: Uint8Array, buf2: Uint8Array) {
+  const merged = new Uint8Array(buf1.length + buf2.length);
+  merged.set(buf1);
+  merged.set(buf2, buf1.length);
+  return merged;
+}
+
 export * as buffer from 'worktop/buffer';
