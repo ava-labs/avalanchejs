f4ae94a1df77719ac528b1ddf5c091e823d22e58
Julian-dev28 <julian_martinez28@outlook.com>
2022-06-03 17:13:16 -0700

    Update getAvaxBalance.ts

--- examples/evm/getAvaxBalance.ts
+++ examples/evm/getAvaxBalance.ts
@@ -21,12 +21,11 @@ const path: string = "/ext/bc/C/rpc"
 const web3 = new Web3(`${protocol}://${ip}:${port}${path}`)
 const cHexAddress: string = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
 
-
 const main = async (): Promise<any> => {
   let balance: BN = await web3.eth.getBalance(cHexAddress)
   balance = new BN(balance.toString())
   const Balance = Web3.utils.fromWei(balance)
-  console.log({ CChainAddress:cHexAddress, Balance: Balance + " AVAX" })
+  console.log({ CChainAddress: cHexAddress, Balance: Balance + " AVAX" })
 }
 
 main()
