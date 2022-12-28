c29412ec1aeed61154c653379e33a95c0738ece9
Gabriel Cardona <gabriel@avalabs.org>
2022-12-28 05:32:30 -0800

    ProofOfPossession toBuffer and fromBuffer serialization with tests.

--- src/apis/platformvm/proofOfPossession.ts
+++ src/apis/platformvm/proofOfPossession.ts
@@ -3,12 +3,20 @@
  * @module API-PlatformVM-ProofOfPossession
  */
 import { Buffer } from "buffer/"
+import BinTools from "../../utils/bintools"
+
+/**
+ * @ignore
+ */
+const bintools: BinTools = BinTools.getInstance()
 
 // A BLS public key and a proof of possession of the key.
 // export class ProofOfPossession extends Serializable {
 export class ProofOfPossession {
   protected _typeName = "ProofOfPossession"
   protected _typeID = undefined
+  protected publicKey: Buffer = Buffer.alloc(48)
+  protected signature: Buffer = Buffer.alloc(96)
 
   /**
    * Returns the {@link https://github.com/feross/buffer|Buffer} representation of the publicKey
@@ -24,8 +32,38 @@ export class ProofOfPossession {
     return this.signature
   }
 
-  protected publicKey: Buffer = Buffer.alloc(48)
-  protected signature: Buffer = Buffer.alloc(96)
+  /**
+  * Takes a {@link https://github.com/feross/buffer|Buffer} containing an [[ProofOfPossession]], parses it, populates the class, and returns the length of the [[ProofOfPossession]] in bytes.
+  *
+  * @param bytes A {@link https://github.com/feross/buffer|Buffer} containing a raw [[ProofOfPossession]]
+  *
+  * @returns The length of the raw [[ProofOfPossession]]
+  *
+  * @remarks assume not-checksummed
+  */
+  fromBuffer(bytes: Buffer, offset: number = 0): number {
+    this.publicKey = bintools.copyFrom(bytes, offset, offset + 48)
+    offset += 48
+
+    this.signature = bintools.copyFrom(bytes, offset, offset + 96)
+    offset += 96
+
+    return offset
+  }
+
+  /**
+  * Returns a {@link https://github.com/feross/buffer|Buffer} representation of the [[ProofOfPossession]]
+  */
+  toBuffer(): Buffer {
+    let bsize: number = this.publicKey.length + this.signature.length
+  
+    const barr: Buffer[] = [
+      this.publicKey,
+      this.signature
+    ]
+  
+    return Buffer.concat(barr, bsize)
+  }
 
   /**
    * Class representing a Proof of Possession
@@ -33,7 +71,7 @@ export class ProofOfPossession {
    * @param publicKey {@link https://github.com/feross/buffer|Buffer} for the public key
    * @param signature {@link https://github.com/feross/buffer|Buffer} for the signature
    */
-  constructor(publicKey: Buffer, signature: Buffer) {
+  constructor(publicKey: Buffer = undefined, signature: Buffer = undefined) {
     this.publicKey = publicKey
     this.signature = signature
   }
