ee49efffc73f9929b8991a3c4358877b9000d526
Emre Kanatli <kanatliemre@gmail.com>
2023-02-16 21:17:23 +0000

    fix: check signers for both address types (#261)
    Co-authored-by: dfenstermaker <dannyfenstermaker@gmail.com>

--- src/vms/common/evmUnsignedTx.ts
+++ src/vms/common/evmUnsignedTx.ts
@@ -1,10 +1,15 @@
 import { publicKeyToEthAddress } from '../../utils';
 import { UnsignedTx } from './unsignedTx';
+import { Address } from '../../serializable/fxs/common';
 
 export class EVMUnsignedTx extends UnsignedTx {
-  protected publicKeyBytesToAddress(pubKey: Uint8Array): Uint8Array {
-    return publicKeyToEthAddress(pubKey);
+  hasPubkey(pubKey: Uint8Array): boolean {
+    const addrAvax = new Address(this.publicKeyBytesToAddress(pubKey));
+    const addrEVM = new Address(publicKeyToEthAddress(pubKey));
+
+    return this.hasAddress(addrAvax) || this.hasAddress(addrEVM);
   }
+
   static fromJSON(jsonStr: string) {
     const tx = UnsignedTx.fromJSON(jsonStr);
     return new EVMUnsignedTx(tx.tx, tx.utxos, tx.addressMaps, tx.credentials);
