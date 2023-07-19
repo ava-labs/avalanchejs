e60b9be9a138e9afd5f75f4ee4841d7c83d56bd6
Lavinia Talpas <laviniatalpas@Lavinias-MacBook-Pro.local>
2023-07-19 19:19:23 +0300

    add comments

--- examples/evm/bech32AddressDecoder.ts
+++ examples/evm/bech32AddressDecoder.ts
@@ -23,6 +23,7 @@ const bech32Decoder = (item: string) => {
 }
 
 const main = async (): Promise<any> => {
+  //can be used for P and X addresses too
   const txID: string = "C-avax1hwgqh0s6yrdy636xv6me5haxecfx99enh5278a"
   const bech32DecodedAddress = bech32Decoder(txID)
   console.log("Decoded address (0x format): " + bech32DecodedAddress)
