19c3e41cbad45c868066da57f7128f5e941f05ea
Lavinia Talpas <laviniatalpas@Lavinias-MacBook-Pro.local>
2023-07-19 20:54:26 +0300

    add error handling + fix stuff

--- examples/evm/bech32AddressDecoder.ts
+++ examples/evm/bech32AddressDecoder.ts
@@ -1,31 +1,30 @@
-import { Buffer } from "../../src"
 import * as bech32 from "bech32"
 import { decode } from "base32-encoding"
+import { Buffer } from "../../src"
 
 const fromDecToHex = (item: number) => {
-  let hexVal = item.toString(16)
-  let hexString = hexVal.length < 2 ? "0" + hexVal : hexVal
-  return hexString
+  const hexVal = item.toString(16)
+  return hexVal.length < 2 ? "0" + hexVal : hexVal
 }
 const bech32Decoder = (item: string) => {
-  let bech32Val = item.length === 45 ? item.split("C-").join("") : item
+  const bech32Val = item.length === 45 ? item.split("C-").join("") : item
   //decode bech32 address into hrp and words
-  let toWords = bech32.bech32.decode(bech32Val)
+  const toWords = bech32.bech32.decode(bech32Val)
   //get buffer from words
   const bufFromWords = Buffer.from(toWords.words)
   //convert words array of unsigned integers from base32 (5-bit) to base256 (8-bit)
-  var base256Converted = decode(bufFromWords)
+  const base256Converted = decode(bufFromWords)
   //get an array of unsigned integers from the buffer obtained
-  let arrBase256Converted = [...base256Converted]
+  const arrBase256Converted = [...base256Converted]
   //convert each integer to its hex value
-  let hexValue = arrBase256Converted.map((item) => fromDecToHex(item))
+  const hexValue = arrBase256Converted.map((item) => fromDecToHex(item))
   return "0x" + hexValue.toString().split(",").join("")
 }
 
 const main = async (): Promise<any> => {
   //can be used for P and X addresses too
-  const txID: string = "C-avax1hwgqh0s6yrdy636xv6me5haxecfx99enh5278a"
-  const bech32DecodedAddress = bech32Decoder(txID)
+  const address: string = "C-avax1hwgqh0s6yrdy636xv6me5haxecfx99enh5278a"
+  const bech32DecodedAddress = bech32Decoder(address)
   console.log("Decoded address (0x format): " + bech32DecodedAddress)
 }
 
