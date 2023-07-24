b34e83f6e22242ea0add8603d25500cfb7b3d953
Lavinia Talpas <laviniatalpas@Lavinias-MacBook-Pro.local>
2023-07-25 02:02:11 +0300

    fix platform.getBalance script

--- examples/platformvm/getBalance.ts
+++ examples/platformvm/getBalance.ts
@@ -9,7 +9,7 @@ const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const pchain: PlatformVMAPI = avalanche.PChain()
 
 const main = async (): Promise<any> => {
-  const address: string = "P-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u"
+  const address: string[] = ["P-local18jma8ppw3nhx5r4ap8clazz0dps7rv5u00z96u"]
   const balance: object = await pchain.getBalance(address)
   console.log(balance)
 }
