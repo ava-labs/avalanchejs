071f7a4cf93b6af0a66b952ef3895d7968a73571
Lavinia Talpas <laviniatalpas@Lavinias-MacBook-Pro.local>
2023-08-08 11:22:55 +0300

    use localhost for keystore apis

--- examples/keystore/listUsers.ts
+++ examples/keystore/listUsers.ts
@@ -2,10 +2,10 @@ import "dotenv/config"
 import { Avalanche } from "../../src"
 import { KeystoreAPI } from "../../src/apis/keystore"
 
-const ip = process.env.IP
-const port = Number(process.env.PORT)
-const protocol = process.env.PROTOCOL
-const networkID = Number(process.env.NETWORK_ID)
+const ip = process.env.LOCAL_IP
+const port = Number(process.env.LOCAL_PORT)
+const protocol = process.env.LOCAL_PROTOCOL
+const networkID = Number(process.env.LOCAL_NETWORK_ID)
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID)
 const keystore: KeystoreAPI = avalanche.NodeKeys()
 
