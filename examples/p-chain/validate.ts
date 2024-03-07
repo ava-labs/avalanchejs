1cad74d179eb294a4e4aac16fd375651f3fe3cd5
bferenc <117105626+bferenc@users.noreply.github.com>
2024-03-07 09:43:00 +0100

    chore(example): add test pop values (#824)

--- examples/p-chain/validate.ts
+++ examples/p-chain/validate.ts
@@ -22,6 +22,12 @@ const main = async () => {
   endTime.setDate(endTime.getDate() + 21);
   const end = BigInt(endTime.getTime() / 1000);
   const nodeID = 'NodeID-HKLp5269LH8DcrLvNDoJquQs2w1LwLCga';
+  const blsPublicKey = hexToBuffer(
+    '0x8f95423f7142d00a48e1014a3de8d28907d420dc33b3052a6dee03a3f2941a393c2351e354704ca66a3fc29870282e15',
+  );
+  const blsSignature = hexToBuffer(
+    '0x86a3ab4c45cfe31cae34c1d06f212434ac71b1be6cfe046c80c162e057614a94a5bc9f1ded1a7029deb0ba4ca7c9b71411e293438691be79c2dbf19d1ca7c3eadb9c756246fc5de5b7b89511c7d7302ae051d9e03d7991138299b5ed6a570a98',
+  );
 
   const tx = newAddPermissionlessValidatorTx(
     context,
@@ -35,6 +41,11 @@ const main = async () => {
     [bech32ToBytes(P_CHAIN_ADDRESS)],
     [bech32ToBytes(P_CHAIN_ADDRESS)],
     1e4 * 20,
+    undefined,
+    1,
+    0n,
+    blsPublicKey,
+    blsSignature,
   );
 
   await addTxSignatures({
