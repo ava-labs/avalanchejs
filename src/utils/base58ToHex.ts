c1b061c6f2ce03e913ae93b49e94fc8a8cfe27a1
Lavinia Talpas <laviniatalpas@Lavinias-MacBook-Pro.local>
2024-05-17 20:48:44 +0300

    feat(utils): add base58 to hex utility function

---
+++ src/utils/base58ToHex.ts
@@ -0,0 +1,17 @@
+import { Buffer } from 'buffer';
+import { base58 } from '@scure/base';
+
+/**
+ * Converts a base58check string to a hex string.
+ * @param {string} base58Str - The base58 string to be converted.
+ * @returns {string} - The hex string.
+ * @throws {Error} - If the conversion fails.
+ */
+export const base58ToHex = (base58Str: string): string => {
+  try {
+    const decoded = base58.decode(base58Str);
+    return '0x' + Buffer.from(decoded).toString('hex');
+  } catch (error) {
+    throw new Error('Failed to convert base58 to hex.');
+  }
+};
