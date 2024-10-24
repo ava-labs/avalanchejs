5710ba7064ff7ced8f363048b4b0fe67d456fc43
Eric Taylor <eric.taylor@avalabs.org>
2024-10-24 12:06:35 -0600

    chore: update ts-jest and jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,19 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+import { createDefaultEsmPreset, type JestConfigWithTsJest } from 'ts-jest';
+
+const preset = createDefaultEsmPreset();
+
+const jestConfig: JestConfigWithTsJest = {
+  ...preset,
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
-  },
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
   coverageProvider: 'v8',
-  extensionsToTreatAsEsm: ['.ts'],
   // Experimental to fix issues with BigInt serialization
   // See: https://jestjs.io/docs/configuration#workerthreads
+  // @ts-expect-error - workerThreads is not in the type definition
   workerThreads: true,
 };
+
+export default jestConfig;
